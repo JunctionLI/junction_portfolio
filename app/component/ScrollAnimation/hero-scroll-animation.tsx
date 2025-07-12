d105509404499ea189fc31fa26b4b69669d80a42
lunian16 <lijunxianjunction@gmail.com>
2025-07-11 22:05:04 -0600

    click event fixed

--- app/component/ScrollAnimation/hero-scroll-animation.tsx
+++ app/component/ScrollAnimation/hero-scroll-animation.tsx
@@ -27,24 +27,6 @@ const Section1: React.FC<SectionProps> = ({ scrollYProgress }) => {
   );
 };
 
-const Section2: React.FC<SectionProps> = ({ scrollYProgress }) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
-
-  return (
-    <motion.section
-      style={{ scale, rotate }}
-      className='relative h-screen '
-    >
-      <div className='absolute bottom-0 left-0 right-0 top-0 z-0'></div>
-      <article className='container mx-auto relative z-10 '>
-        <div className='grid grid-cols-4 gap-4'>
-            
-        </div>
-      </article>
-    </motion.section>
-  );
-};
 
 const Component = forwardRef<HTMLElement>((props, ref) => {
   const container = useRef<HTMLDivElement>(null);
@@ -57,7 +39,7 @@ const Component = forwardRef<HTMLElement>((props, ref) => {
     <>
       <main ref={container} className='relative h-[200vh]'>
         <Section1 scrollYProgress={scrollYProgress} />
-        <Section2 scrollYProgress={scrollYProgress} />
+       
       </main>
     </>
   );
